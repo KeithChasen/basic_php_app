04a1b33af04511d810153b59cf942fe241c1202a
keithchasen <keithchasen89@gmail.com>
2019-06-03 18:02:21 +0300

    basic PDO set up

---
+++ helpers.php
@@ -0,0 +1,14 @@
+<?php
+
+function pd($item) {
+    echo "<pre>" . print_r($item,1) . "</pre>";
+}
+
+function printException(Exception $e) {
+    echo $e->getMessage();
+    echo "<br>";
+    echo $e->getFile();
+    echo "<br>";
+    echo $e->getLine();
+    echo "<br>";
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,26 @@
 <?php
 
-phpinfo();
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../helpers.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::create(__DIR__ . '/../');
+$dotenv->load();
+
+$driver = getenv('NETCE_DB_DRIVER');
+$host = getenv('NETCE_DB_HOST');
+$dname = getenv('NETCE_DB_NAME');
+$port = getenv('NETCE_DB_PORT');
+$dbusername = getenv('NETCE_DB_USER');
+$dbpassword = getenv('NETCE_DB_PASSWORD');
+
+$dsn = "{$driver}:host={$host};dbname={$dname};port={$port}";
+
+try {
+    $pdo = new PDO($dsn,$dbusername,$dbpassword);
+} catch (Exception $e) {
+    printException($e);
+}
+
+
