6699e54680c2a3060312e7350a13656fbfe2454f
keithchasen <keithchasen89@gmail.com>
2019-06-12 14:22:40 +0300

    login route

--- app/AuthController.php
+++ app/AuthController.php
@@ -27,12 +27,29 @@ class AuthController
         $email = $request->get('email');
         $password = $request->get('password');
 
-        $this->authService->register($email, $password);
-
-        $message = '';
+        $result = $this->authService->register($email, $password);
 
         return new Response(
-            $message
+            json_encode($result)
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     *
+     * method: POST
+     * route: /auth/login
+     */
+    public function login(Request $request)
+    {
+        $email = $request->get('email');
+        $password = $request->get('password');
+
+        $result = $this->authService->login($email, $password);
+
+        return new Response(
+            json_encode($result)
         );
     }
 
--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -6,22 +6,97 @@ use Illuminate\Hashing\BcryptHasher;
 
 class AuthService extends BasicService
 {
+    private $bcrypt;
+
+    /**
+     * AuthService constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->bcrypt = new BcryptHasher();
+    }
+
+    /**
+     * @param $email
+     * @param $password
+     * @return array
+     */
     public function register($email, $password)
     {
-        //todo: check if user with provided email already exists;
+        $userExists = $this->getUserByEmail($email);
+
+        if (!empty($userExists)) {
+            return [
+                'ok' => false,
+                'message' => 'User with such email already exists. Please log in.'
+            ];
+        }
+
+        $password = $this->bcrypt->make($password);
 
         $pdo = $this->getPdo();
-
         $sql = "INSERT INTO users (email, password) VALUES (?, ?)";
-
         $newUser = $pdo->prepare($sql);
-
-        $bcrypt = new BcryptHasher();
-        $password = $bcrypt->make($password);
-
         $newUser->execute([$email, $password]);
 
-        return $pdo->lastInsertId();
+        if ($pdo->lastInsertId()) {
+            return [
+                'ok' => true,
+                'message' => 'Successfully registered. Now you can log in.'
+            ];
+        }
+
+        return [
+            'ok' => false,
+            'message' => 'Something went wrong. Please try again'
+        ];
+
+    }
+
+    /**
+     * @param $email
+     * @param $password
+     * @return array
+     */
+    public function login($email, $password)
+    {
+        $user = $this->getUserByEmail($email);
+
+        $passwordsMatch = $this->bcrypt->check($password, $user['password']);
+
+        if ($passwordsMatch) {
+            return [
+                'ok' => true,
+                'user' => [
+                    'id' => $user['id'],
+                    'email' => $user['email']
+                ],
+                'message' => 'Successfully logged in'
+            ];
+        }
+
+        return [
+            'ok' => false,
+            'user' => null,
+            'message' => 'Credentials might be wrong'
+        ];
+    }
+
+    /**
+     * @param $email
+     * @return mixed
+     */
+    protected function getUserByEmail($email)
+    {
+        $pdo = $this->getPdo();
+
+        $sql = "SELECT * FROM users WHERE email = ?";
+
+        $attemptLogin = $pdo->prepare($sql);
+        $attemptLogin->execute([$email]);
+
+        return $attemptLogin->fetch($pdo::FETCH_ASSOC);
     }
 
 }
